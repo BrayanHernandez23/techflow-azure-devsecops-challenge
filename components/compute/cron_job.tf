cc4b63981326b3119f5c6e76e16fd2a9d8be77a5
Brayan Daniel Tique Hernandez <BTIQUE@bancodebogota.com.co>
2026-02-11 16:29:06 -0500

    🚀 add missing compute resource arguments and identity to container jobs

--- components/compute/cron_job.tf
+++ components/compute/cron_job.tf
@@ -3,6 +3,13 @@ resource "azurerm_container_app_job" "cleanup" {
   location                     = var.location
   resource_group_name          = var.resource_group_name
   container_app_environment_id = azurerm_container_app_environment.env.id
+  replica_timeout_in_seconds   = 300
+  replica_retry_limit          = 1
+
+  identity {
+    type         = "UserAssigned"
+    identity_ids = [var.user_assigned_identity_id]
+  }
 
   schedule_trigger_config {
     cron_expression = "0 0 * * *"
@@ -13,6 +20,8 @@ resource "azurerm_container_app_job" "cleanup" {
       image   = "alpine:latest"
       name    = "worker"
       command = ["/bin/sh", "-c", "echo 'Job ejecutado con éxito'"]
+      cpu     = 0.25
+      memory  = "0.5Gi"
     }
   }
 }
