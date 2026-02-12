8f8f07484671e6bd85d1c2af09b27e7764abffe5
Brayan Daniel Tique Hernandez <BTIQUE@bancodebogota.com.co>
2026-02-12 14:14:46 -0500

    🔧 use specific secret version and force public access

--- components/security/azure_key_vault.tf
+++ components/security/azure_key_vault.tf
@@ -20,6 +20,7 @@ resource "azurerm_key_vault" "kv" {
   sku_name                   = "standard"
   soft_delete_retention_days = 7
   purge_protection_enabled   = true
+  public_network_access_enabled = true
   # checkov:skip=CKV_AZURE_189: Firewall set to 'Allow' mode to ensure seamless access from the CI/CD pipeline.
   # checkov:skip=CKV_AZURE_109: Network simplification for the technical evaluation environment.
   # checkov:skip=CKV2_AZURE_32: Private Endpoint not implemented to reduce networking complexity for this challenge.
--- components/security/outputs.tf
+++ components/security/outputs.tf
@@ -6,7 +6,7 @@ output "identity_id" {
 
 output "secret_id" {
   description = "The Versionless ID of the Key Vault secret. Using the versionless ID ensures the application always retrieves the latest enabled version of the secret."
-  value       = azurerm_key_vault_secret.app_secret.versionless_id
+  value       = azurerm_key_vault_secret.app_secret.id
   depends_on  = [time_sleep.wait_180_seconds]
 }
 
