c10b89654377ddfde48eca20ef3b0714cc56c147
Brayan Daniel Tique Hernandez <BTIQUE@bancodebogota.com.co>
2026-02-12 12:44:53 -0500

    ⏳ increase propagation delay to 180s for managed identity consistency

--- components/security/azure_key_vault.tf
+++ components/security/azure_key_vault.tf
@@ -44,11 +44,11 @@ resource "azurerm_key_vault_secret" "app_secret" {
   expiration_date = "2026-12-31T23:59:59Z"
 }
 
-resource "time_sleep" "wait_90_seconds" {
+resource "time_sleep" "wait_180_seconds" {
   depends_on = [
     azurerm_key_vault.kv,
     azurerm_user_assigned_identity.app_identity,
     azurerm_key_vault_secret.app_secret
   ]
-  create_duration = "90s"
+  create_duration = "180s"
 }
