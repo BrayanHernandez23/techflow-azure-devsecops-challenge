11288aef6ed5f94adff6b916fee8c852aba3738e
Brayan Daniel Tique Hernandez <BTIQUE@bancodebogota.com.co>
2026-02-12 08:18:05 -0500

    🔐 add secret recovery permissions to Key Vault access policy

--- components/security/azure_key_vault.tf
+++ components/security/azure_key_vault.tf
@@ -28,7 +28,7 @@ resource "azurerm_key_vault" "kv" {
     object_id          = data.azurerm_client_config.current.object_id
     secret_permissions = ["Get", "List", "Set", "Delete", "Purge", "Recover", "Restore"]
   }
-  
+
   access_policy {
     tenant_id          = data.azurerm_client_config.current.tenant_id
     object_id          = azurerm_user_assigned_identity.app_identity.principal_id
